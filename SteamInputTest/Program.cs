848d165076a60021c1861ebb0f5dbee5cee3ef77
Matthew Suddaby <msuddaby2@nait.ca>
2024-11-15 11:30:58 -0700

    add brainflow stuff

--- SteamInputTest/Program.cs
+++ SteamInputTest/Program.cs
@@ -1,4 +1,6 @@
-﻿using Nefarius.ViGEm.Client;
+﻿using brainflow;
+using brainflow.math;
+using Nefarius.ViGEm.Client;
 using Nefarius.ViGEm.Client.Targets;
 using Nefarius.ViGEm.Client.Targets.DualShock4;
 using Nefarius.ViGEm.Client.Targets.Xbox360;
@@ -7,51 +9,70 @@ namespace SteamInputTest;
 
 class Program
 {
+    private static readonly int _boardId = (int)BoardIds.GANGLION_BOARD;
+    private static readonly BrainFlowInputParams _inputParams = new BrainFlowInputParams
+    {
+        serial_port = "COM10"
+    };
+    
     static void Main(string[] args)
     {
         var client = new ViGEmClient();
-
-        var ds4 = client.CreateDualShock4Controller();
-
-        ds4.Connect();
-        Console.WriteLine("Connected to DS4");
+        var controller = client.CreateXbox360Controller();
         
-        Task.Run(() => MonitorOutputReports(ds4));
+        controller.Connect();
+        Console.WriteLine("Xbox 360 controller connected");
+        
+        BoardShim.enable_dev_board_logger();
+        
+        BoardShim boardShim = new BoardShim(_boardId, _inputParams);
+        
+        BoardControlLoop(boardShim);
+    }
 
-        while (true)
+    static void BoardControlLoop(BoardShim boardShim)
+    {
+        
+        boardShim.prepare_session();
+        
+        // Enable accelerometer
+        boardShim.config_board("n");
+        
+        boardShim.start_stream();
+        
+        Thread.Sleep(5000);
+        
+        double[,] unprocessedData = boardShim.get_current_board_data(1);
+        int[] eeg_channels = BoardShim.get_eeg_channels(_boardId);
+        int[] accel_channels = BoardShim.get_accel_channels(_boardId);
+
+        foreach (var index in eeg_channels)
         {
-            Console.WriteLine("Press any key to press Cross");
-            var input = Console.ReadKey(intercept: true).Key;
-
-            if (input == ConsoleKey.Escape)
-            {
-                break;
-            }
-
-            if (input == ConsoleKey.Enter)
-            {
-                Thread.Sleep(2000);
-                ds4.SetButtonState(DualShock4Button.Cross, true);
-                ds4.SubmitReport();
-                Console.WriteLine("Cross pressed");
-
-                
-            }
-
-            if (input == ConsoleKey.LeftArrow)
-            {
-                ds4.SetAxisValue(DualShock4Axis.LeftThumbX, 0);
-                ds4.SubmitReport();
-                
-                Thread.Sleep(2000);
-                
-                ds4.SetAxisValue(DualShock4Axis.LeftThumbX, 128);
-                ds4.SubmitReport();
-            }
+            var row = unprocessedData.GetRow(index);
+            
+            
+            // if (row[index] == match_action_value)
+            // {
+            //     controller.SetButtonState(Xbox360Button.A, true);
+            //     controller.SubmitReport();
+            // }
+            
+            
+            
         }
 
-        ds4.Disconnect();
-    }
+        foreach (var index in accel_channels)
+        {
+            // SetAxisValue - 0 is left, 128 is middle, 255 is right
+            // if (row[index] == match_move_value)
+            // {
+            //     controller.SetAxisValue(Xbox360Axis.LeftThumbX, 0);
+            //     controller.SubmitReport();
+            // }
+        }
+    } 
+
+    
     
     
     static void MonitorOutputReports(IDualShock4Controller ds4)
